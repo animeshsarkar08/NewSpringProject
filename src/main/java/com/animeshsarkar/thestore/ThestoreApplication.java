bd1c1c0be822afdd7e440ad76f55b011a5ea3322
animeshsarkar08 <sarkaranimesh05@gmail.com>
2024-11-10 11:30:28 +0530

    new

---
+++ src/main/java/com/animeshsarkar/thestore/ThestoreApplication.java
@@ -0,0 +1,13 @@
+package com.animeshsarkar.thestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animeshsarkar/thestore/ThestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeshsarkar.thestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
